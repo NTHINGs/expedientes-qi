4bb61b661d55d705304a4f9e69a4a2892f613f96
Mauricio Martinez <n_othing@hotmail.com>
2018-09-22 12:34:57 -0500

    Probando pdf

--- js/expedientes_qi.js
+++ js/expedientes_qi.js
@@ -1,3 +1,9 @@
 function expedientes_qi_imprimir(mode, expedientes) {
-    alert(window.atob(expedientes));
+    let patient = JSON.parse(window.atob(expedientes));
+
+    let doc = new jsPDF()
+
+    doc.text('Hello world!', 10, 10)
+    doc.text(patient.nombre, 15, 15);
+    doc.save('a4.pdf')
 }
