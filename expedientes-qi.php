9d1c1cb6b0527a3b653ce04a1c467fe6722bd85a
Mauricio Martinez <n_othing@hotmail.com>
2018-09-26 16:38:48 -0500

    Formulario de agregar paciente

--- expedientes-qi.php
+++ expedientes-qi.php
@@ -56,8 +56,10 @@ if ( file_exists( ABS_DIR . '/shortcodes/agregar-paciente.php' ) ) {
 add_action('wp_enqueue_scripts','expedientes_qi_init');
 
 function expedientes_qi_init() {
+	wp_enqueue_script( 'gijgo', '//cdn.jsdelivr.net/npm/gijgo@1.9.10/js/gijgo.min.js', array( 'jquery' ), '3.3.1', true );
+	wp_enqueue_style( 'gijgo', '//cdn.jsdelivr.net/npm/gijgo@1.9.10/css/gijgo.min.css');
     wp_enqueue_script( 'jspdf', plugins_url( '/js/jspdf.min.js', __FILE__ ));
-	wp_enqueue_script( 'expedientes_qi', plugins_url( '/js/expedientes_qi.js', __FILE__ ));
+	wp_enqueue_script( 'expedientes_qi', plugins_url( '/js/expedientes_qi.js', __FILE__ ), array( 'jquery' ), false, true );
 }
 
 // Create Tables
