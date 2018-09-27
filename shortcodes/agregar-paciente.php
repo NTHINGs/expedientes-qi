fff9a22c65ba338b7ba1c945d7d41425daccd490
Mauricio Martinez <n_othing@hotmail.com>
2018-09-27 17:27:08 -0500

    Paciente form

--- shortcodes/agregar-paciente.php
+++ shortcodes/agregar-paciente.php
@@ -41,7 +41,7 @@ if ( ! function_exists( 'agregar_paciente_shortcode' ) ) {
         }
     
         if ( isset( $_POST['submitted'] ) ) {
-            $uploadedfile = $_FILES['file'];
+            $uploadedfile = $_FILES['fotografia'];
             $upload_overrides = array( 'test_form' => false );
             $movefile = wp_handle_upload( $uploadedfile, $upload_overrides );
             if ( $movefile && ! isset( $movefile['error'] ) ) {
